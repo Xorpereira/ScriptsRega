cd47f077d69f5ed3dffbcc944a4657df24e7d0b8
Xorpereira <32929861+Xorpereira@users.noreply.github.com>
2017-10-19 14:04:53 +0100

    translated comments

--- outputlcd.py
+++ outputlcd.py
@@ -6,12 +6,12 @@ import time
 
 lcdi2c = I2C_LCD_driver.lcd()
 
-#Exibe informacoes iniciais
+#Initial info
 lcdi2c.lcd_display_string("Esteirense", 1,1)
 lcdi2c.lcd_display_string("LCD I2C e RPi", 2,1)
 time.sleep(2)
 
-#Apaga o display
+#Clear display
 lcdi2c.lcd_clear()
 
 def get_ip_address(ifname):
@@ -22,12 +22,12 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
-#Mostra o endereco IP
+#Show IP (3g modem)
 lcdi2c.lcd_display_string("IP", 1)
 lcdi2c.lcd_display_string(get_ip_address('ppp0'), 1,3)
  
 while True:
-#Mostra a data no display
+#Show date
     lcdi2c.lcd_display_string("Data: %s" %time.strftime("%d/%m/%y"), 2,1)
 
 time.sleep(5)
