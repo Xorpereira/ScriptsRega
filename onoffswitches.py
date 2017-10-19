1c58f59b3c12acd4838463e11c642cd17d95eb4f
Xorpereira <32929861+Xorpereira@users.noreply.github.com>
2017-10-19 13:51:42 +0100

    changed var name

--- onoffswitches.py
+++ onoffswitches.py
@@ -7,7 +7,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup([5,6,13,19],GPIO.IN, pull_up_down=GPIO.PUD_DOWN);
 
 
-estadoanterior = [0, 0, 0, 0];
+prevstate = [0, 0, 0, 0];
 input =[0, 0, 0, 0]; 
 var = 1;
 
@@ -19,40 +19,40 @@ while var == 1 :
 	input[2] = GPIO.input(13);
 	input[3] = GPIO.input(19);
 
-	if ( estadoanterior[0] != input[0] ) :
+	if ( prevstate[0] != input[0] ) :
 		print "Button 1 Pressed"
 		if ( input[0] == 1 ) :
                         r=requests.get("http://esteirense:8080/bin/manual?zone=zc&state=on")
 		else :
                         r=requests.get("http://esteirense:8080/bin/manual?zone=zc&state=off")
                         r=requests.get("http://esteirense:8080/bin/run?system=on")
-                estadoanterior[0] = input[0];
-		print "estadoanterior1 alterado: ", estadoanterior[0]
-	if ( estadoanterior[1] != input[1] ) :
+                prevstate[0] = input[0];
+		print "prevstate1 alterado: ", prevstate[0]
+	if ( prevstate[1] != input[1] ) :
 		print "Button 2 Pressed"
 		if ( input[1] == 1 ) :
                         r=requests.get("http://esteirense:8080/bin/manual?zone=zd&state=on")
 		else :
                         r=requests.get("http://esteirense:8080/bin/manual?zone=zd&state=off")
                         r=requests.get("http://esteirense:8080/bin/run?system=on")
-                estadoanterior[1] = input[1];
-		print "estadoanterior2 alterado: ", estadoanterior[1]
-	if ( estadoanterior[2] != input[2] ) :
+                prevstate[1] = input[1];
+		print "prevstate2 alterado: ", prevstate[1]
+	if ( prevstate[2] != input[2] ) :
 		print "Button 3 Pressed"
 		if ( input[2] == 1 ) :
                         r=requests.get("http://esteirense:8080/bin/manual?zone=zh&state=on")
 		else :
                         r=requests.get("http://esteirense:8080/bin/manual?zone=zh&state=off")
                         r=requests.get("http://esteirense:8080/bin/run?system=on")
-                estadoanterior[2] = input[2];
-		print "estadoanterior3 alterado: ", estadoanterior[2]
-	if ( estadoanterior[3] != input[3] ) :
+                prevstate[2] = input[2];
+		print "prevstate3 alterado: ", prevstate[2]
+	if ( prevstate[3] != input[3] ) :
 		print "Button 4 Pressed"
 		if ( input[3] == 1 ) :
                         r=requests.get("http://esteirense:8080/bin/manual?zone=zm&state=on")
 		else :
                         r=requests.get("http://esteirense:8080/bin/manual?zone=zm&state=off")
                         r=requests.get("http://esteirense:8080/bin/run?system=on")
-                estadoanterior[3] = input[3];
-		print "estadoanterior4 alterado: ", estadoanterior[3]
+                prevstate[3] = input[3];
+		print "prevstate4 alterado: ", prevstate[3]
 	time.sleep (1)
