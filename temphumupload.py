73a814a61717ad2ea172ba2e37cacab5cbfb081f
Xorpereira <32929861+Xorpereira@users.noreply.github.com>
2017-10-20 16:55:52 +0100

    errors

--- temphumupload.py
+++ temphumupload.py
@@ -28,6 +28,6 @@ if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 r=requests.get("http://rtupdate.wunderground.com/weatherstation/updateweatherstation.php?ID=IPAMPILH2&PASSWORD=u0dgvs0f&dateutc=now&humidity={1:0.1f}&tempc={0:0.1f}&realtime=1&rtfreq=10&action=updateraw".format(temperature, humidity))
 	
-else:
-	print('Failed to get reading. Try again!')
+#else:
+#	print('Failed to get reading. Try again!')
 
