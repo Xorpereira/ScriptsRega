a753b7176504ecd009119d7eb5bb2b19fa11f998
Xorpereira <32929861+Xorpereira@users.noreply.github.com>
2017-10-20 17:32:24 +0100

    added loop

--- temphumupload.py
+++ temphumupload.py
@@ -4,6 +4,10 @@ import Adafruit_DHT
 import requests
 import time
 
+var=1
+#Refreshrate in minutes
+refreshrate=1
+
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 sensor = Adafruit_DHT.AM2302
@@ -16,21 +20,23 @@ sensor = Adafruit_DHT.AM2302
 # connected to GPIO23.
 pin = 12
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+while var == 1
 
-#Fahrenheit convertion
-temperature=temperature*9/5.0+32
+	# Try to grab a sensor reading.  Use the read_retry method which will retry up
+	# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-r=requests.get("http://rtupdate.wunderground.com/weatherstation/updateweatherstation.php?ID=IPAMPILH2&PASSWORD=u0dgvs0f&dateutc=now&humidity={1:0.1f}&tempf={0:0.1f}&realtime=1&rtfreq=10&action=updateraw".format(temperature, humidity))
-	
-#else:
-#	print('Failed to get reading. Try again!')
+	#Fahrenheit convertion
+	temperature=temperature*9/5.0+32
 
+	# Note that sometimes you won't get a reading and
+	# the results will be null (because Linux can't
+	# guarantee the timing of calls to read the sensor).
+	# If this happens try again!
+	if humidity is not None and temperature is not None:
+		#print('Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature, humidity))
+		r=requests.get("http://rtupdate.wunderground.com/weatherstation/updateweatherstation.php?ID=IPAMPILH2&PASSWORD=u0dgvs0f&dateutc=now&humidity={1:0.1f}&tempf={0:0.1f}&realtime=1&rtfreq=10&action=updateraw".format(temperature, humidity))
+	#else:
+	#	print('Failed to get reading. Try again!')
+	#Time until next probe/upload
+	time.sleep(60*refreshrate)
